c3f6d7ca8e01848bdcffed0fad9231a4a466bb97
20181102931 <18647197975@163.com>
2019-06-19 21:26:13 +0800

    youyou

--- wrj/main.cpp
+++ wrj/main.cpp
@@ -7,9 +7,60 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include<fstream>
+using namespace std;
+struct message
+{
+    char a;
+    string name[3];
+    string cname[7];
+    double score[];
 }
+
+int main()
+{
+    char a;
+    string name[3];
+    string cname;
+    double score[7];
+    double sum=0;
+    double ave=0;
+    ifstream file("/Users/s20181102931/Desktop/wrj.txt" );
+    cin>>a>>name;
+    ifstream mmm("/Users/s20181102931/Desktop/we.txt");
+    cin>>cname;
+//冒泡排序去掉最大值，最小值，取平均值；
+    for(int i=0;i<7;i++)
+    {
+        cin>>score[i];
+    }
+    for (int i=0;i<6;i++)
+    {
+        for (int j=0;j<6-i;j++)
+        {
+            if (score[j] > score[j + 1])
+            {
+                int tem = score[j];
+                score[j] = score[j + 1];
+                score[j + 1] = temp;
+            }
+        }
+    }
+    for(int i=1;i<6;i++)
+    {
+        sum=sum+score[i];
+    }
+      ave=sum/7;
+    
+    
+    
+        if (file.is_open())
+        {
+            cout<<
+            file.close();
+        }
+        
+        return 0;
+    }
+
+
